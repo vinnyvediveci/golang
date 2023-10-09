4511340b76b9611a41c1f7b16c71a4b8a4e6cf1e
Vinul Witharanage <vndwitharanage@gmail.com>
2023-10-09 01:25:31 +0100

    2d slices

--- slices.go
+++ slices.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-//	"slices"
+	"slices"
 )
 
 func main() {
@@ -28,4 +28,31 @@ func main() {
 	c := make([]string, len(s))
 	copy(c, s)
 	fmt.Println("copy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+	
+	m := s[:5]
+	fmt.Println("sl2:", m)
+	
+	n := s[2:]
+	fmt.Println("sl3:", n)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+// Slices can be composed into multi-dimensional data structures. The length of the inner slices can vary, unlike with multi-dimensional arrays.
+    twoD := make([][]int, 3)
+    for i := 0; i < 3; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
 } 
