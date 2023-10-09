328c3ea7f3ae0888ee400717760a44ae9d960b13
Vinul Witharanage <vndwitharanage@gmail.com>
2023-10-09 01:10:52 +0100

    slices, set, get, make and copy

---
+++ slices.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+//	"slices"
+)
+
+func main() {
+
+	var s []string
+	fmt.Println("uninit: ", s, s == nil, len(s) == 0, "cap:", cap(s))
+
+ 	s = make([]string, 3)
+	fmt.Println("emp: ",s , "len: ", len(s), "cap: ", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apnd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("copy:", c)
+} 
