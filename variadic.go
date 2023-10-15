b32c4f1670e21899b21dd022e3dbc9b2cec14159
Vinul Witharanage <vndwitharanage@gmail.com>
2023-10-15 21:46:00 +0100

    variadic

---
+++ variadic.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int){
+	fmt.Print(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	
+	fmt.Println(total)
+}
+
+func main(){
+	sum(1,3,4)
+	sum(1,3,5,4)
+	
+	nums := []int{1,2,3,4,5}
+	sum(nums...)
+
+}
